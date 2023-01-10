fbdaae8f458455bc685a6966a204939e1bd1602a
iFaxity <christian@faxity.se>
2023-01-11 00:58:33 +0100

    build: Cleanup in build config

--- build.config.ts
+++ build.config.ts
@@ -2,11 +2,7 @@ import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
   entries: [
-    'src/index'
-  ],
-  externals: [
-    'semantic-release',
-    'execa',
+    'src/index',
   ],
   clean: true,
   declaration: true,
