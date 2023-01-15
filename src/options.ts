a694e34d2f9e071137121ac1462d06fbf1ea0279
iFaxity <christian@faxity.se>
2023-01-16 00:21:41 +0100

    feat: Changed NUGET_TOKEN env var to NUGET_API_KEY

--- src/options.ts
+++ src/options.ts
@@ -43,7 +43,7 @@ export async function resolveOptions(
 
     return PluginOptions.parseAsync({
         source: env.NUGET_SOURCE,
-        apiKey: env.NUGET_TOKEN,
+        apiKey: env.NUGET_API_KEY,
         ...options,
         properties,
     });
