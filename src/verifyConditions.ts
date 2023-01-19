a0ed8cdb5e15d9d133a74ca59dde9746c4a91999
iFaxity <christian@faxity.se>
2023-01-20 00:47:09 +0100

    feat: Added verification of dotnet-cli and prints the dotnet version

--- src/verifyConditions.ts
+++ src/verifyConditions.ts
@@ -3,6 +3,7 @@ import { PluginOptions, resolveOptions } from './options';
 import { execPipe } from './execPipe';
 
 const NUGET_VERSION_REGEX = /^NuGet Command Line\s+([0-9.]+)$/m;
+const DOTNET_CLI_VERSION_REGEX = /^([0-9.]+)$/g;
 
 export async function verifyConditions(
   options: Config & PluginOptions,
@@ -16,18 +17,8 @@ export async function verifyConditions(
   // Set up debugging.
   const { logger } = context;
 
-  logger.debug('options', resolved);
-
-  if (!resolved.source) {
-    errors.push('NUGET_SOURCE or the push.source option must be set!');
-  }
-
-  if (!resolved.apiKey) {
-    errors.push('NUGET_TOKEN or the push.apiKey option must be set!');
-  }
-
-  // Make sure we have access to the `dotnet nuget` command.
-  try {
+  async function verifyNuget() {
+    // Make sure we have access to the `dotnet nuget` command.
     const output = await execPipe('dotnet', [
       'nuget',
       '--version',
@@ -45,6 +36,41 @@ export async function verifyConditions(
         logger.warn(`'dotnet nuget --version' didn't respond as expected!`);
       }
     }
+  }
+
+  async function verifyDotnet() {
+    // Make sure we have access to the `dotnet` command.
+    const output = await execPipe('dotnet', [
+      '--version',
+    ], options);
+
+    if (output.failed) {
+      errors.push(`The 'dotnet --version' responded with an error code`);
+    } else {
+      const { stdout } = output;
+      const match = DOTNET_CLI_VERSION_REGEX.exec(stdout);
+
+      if (match != null && match.length == 2) {
+        logger.info(`Using dotnet-cli version ${match[1]}`);
+      } else {
+        logger.warn(`'dotnet --version' didn't respond as expected: ${stdout}`);
+      }
+    }
+  }
+
+  logger.debug('options', resolved);
+
+  if (!resolved.source) {
+    errors.push(`NUGET_SOURCE or the 'source' option must be set!`);
+  }
+
+  if (!resolved.apiKey) {
+    errors.push(`NUGET_API_KEY or the 'apiKey' option must be set!`);
+  }
+
+  try {
+    await verifyDotnet();
+    await verifyNuget();
   } catch {
     errors.push(`Cannot run the 'dotnet' process!`);
   }
@@ -55,3 +81,5 @@ export async function verifyConditions(
     throw new Error('Could not verify conditions for NuGet!');
   }
 }
+
+
